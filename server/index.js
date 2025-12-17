f623834b62894c2735d7de25f159f80989ffc8e0
Rizwan2611 <rizxx50@gmail.com>
2025-12-17 14:43:21 +0530

    Fix: Revert Express route wildcard to simple star

--- server/index.js
+++ server/index.js
@@ -50,7 +50,8 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('/:pathMatch(.*)', (req, res) => {
+  // Fallback to index.html for SPA
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
